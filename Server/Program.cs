be2c95479380e1cfc4730b5363723676828eb955
markovicdjordje <djmarkovic51@gmail.com>
2026-05-05 17:37:09 +0200

    NetTcpBinding object

--- Server/Program.cs
+++ Server/Program.cs
@@ -3,13 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ServiceModel;
+using Server.Services;
+using Shared.Contracts;
+using System.ServiceModel.Channels;
 
 namespace Server
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
+            NetTcpBinding binding = new NetTcpBinding();
+            binding.TransferMode = TransferMode.Streamed;
+            binding.MaxReceivedMessageSize = 10 * 1024 * 1024;
+            binding.SendTimeout = new TimeSpan(0, 10, 0);
+            binding.ReceiveTimeout = new TimeSpan(0, 10, 0);
         }
     }
 }
