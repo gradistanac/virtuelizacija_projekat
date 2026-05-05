714973a717393747f1a7bb4324af39c24b4024d9
markovicdjordje <djmarkovic51@gmail.com>
2026-05-05 17:21:08 +0200

    Implemented IEegService

---
+++ Server/Services/EegService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Shared.Contracts;
+using Shared.Models;
+
+namespace Server.Services
+{
+    public class EegService : IEegService
+    {
+        public void EndSession()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string PushSample(EegSample sample)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string StartSession(EegMeta meta)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
