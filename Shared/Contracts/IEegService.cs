c75d70795087d8a8898774cbb170abd1d8426fb2
markovicdjordje <djmarkovic51@gmail.com>
2026-05-05 17:14:17 +0200

    New Shared project

--- Shared/Contracts/IEegService.cs
+++ Shared/Contracts/IEegService.cs
@@ -3,10 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using Shared.Models;
 
 namespace Shared.Contracts
 {
-    internal interface IEegService
+    [ServiceContract]
+    public interface IEegService
     {
+        [OperationContract]
+        string StartSession(EegMeta meta);
+        [OperationContract]
+        string PushSample(EegSample sample);
+        [OperationContract]
+        void EndSession();
     }
 }
--- Shared/Models/EegMeta.cs
+++ Shared/Models/EegMeta.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Shared.Models
 {
-    internal class EegMeta
+    public class EegMeta
     {
     }
 }
