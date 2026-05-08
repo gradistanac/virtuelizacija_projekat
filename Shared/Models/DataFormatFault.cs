e6cbc774801acea87a435dbf468d011bc0e40757
markovicdjordje <djmarkovic51@gmail.com>
2026-05-09 00:23:26 +0200

    Standardized exceptions

---
+++ Shared/Models/DataFormatFault.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Models
+{
+    [DataContract]
+    public class DataFormatFault
+    {
+        [DataMember]
+        public string Message { get; set; }
+    }
+}
