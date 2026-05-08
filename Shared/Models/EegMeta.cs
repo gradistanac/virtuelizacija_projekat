dd9add6f08cbce3229ea8a23da70a350e5e046ad
markovicdjordje <djmarkovic51@gmail.com>
2026-05-09 00:23:41 +0200

    Implemented EegMeta class

--- Shared/Models/EegMeta.cs
+++ Shared/Models/EegMeta.cs
@@ -1,12 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.ServiceModel;
 using System.Text;
 using System.Threading.Tasks;
+using System.Runtime.Serialization;
 
 namespace Shared.Models
 {
+    [DataContract]
     public class EegMeta
     {
+        [DataMember]
+        public string ParticipantId { get; set; }
+        [DataMember]
+        public string FileName { get; set; }
+        [DataMember]
+        public int TotalRows { get; set; }
+        [DataMember]
+        public string SchemaVersion { get; set; }
     }
 }
