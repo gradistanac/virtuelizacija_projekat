530142b4f22335a7105535b99168188a86a43430
markovicdjordje <djmarkovic51@gmail.com>
2026-05-08 23:08:56 +0200

    Test connection

--- Client/Program.cs
+++ Client/Program.cs
@@ -3,13 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ServiceModel;
+using Shared.Contracts;
+using Shared.Models;
 
 namespace Client
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
+            ChannelFactory<IEegService> factory =
+               new ChannelFactory<IEegService>("EegServiceEndpoint");
+
+            IEegService proxy = factory.CreateChannel();
+
+            try
+            {
+                string odgovor = proxy.Ping("Zdravo servere");
+
+                Console.WriteLine(odgovor);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.ReadLine();
         }
     }
 }
